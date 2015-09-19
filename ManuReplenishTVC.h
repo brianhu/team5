977e366b25424452471873db64e8ddfe85e6f968
CoSNaYe <c.cosnaye@gmail.com>
2015-09-19 17:07:16 +0800

    start doing "replenish ingredient"

---
+++ ManuReplenishTVC.h
@@ -0,0 +1,13 @@
+//
+//  ManuReplenishTVC.h
+//  TeamFive
+//
+//  Created by 陳逸仁 on 9/19/15.
+//  Copyright © 2015 yishain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ManuReplenishTVC : UITableViewController
+
+@end
