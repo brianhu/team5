3d13ea6c9f0c9bc2613a3ae688b8d5f8561068e7
Tanklin <tanknmm@gmail.com>
2015-09-19 18:40:47 +0800

    Add CollectionView's Cell and Register a nib; Add a image and pull a ImageView to StoryBoard.

---
+++ SettingsCollectionViewCell.m
@@ -0,0 +1,17 @@
+//
+//  SettingsCollectionViewCell.m
+//  TeamFive
+//
+//  Created by Tank Lin on 2015/9/19.
+//  Copyright © 2015年 yishain. All rights reserved.
+//
+
+#import "SettingsCollectionViewCell.h"
+
+@implementation SettingsCollectionViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
--- SettingsCollectionViewController.h
+++ SettingsCollectionViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SettingsCollectionViewCell.h"
 
 @interface SettingsCollectionViewController : UICollectionViewController
 
