b2ce7340cf32fddf7df7d0840ff5427854294a03
CoSNaYe <c.cosnaye@gmail.com>
2015-09-20 14:59:43 +0800

    split stock into two groups

--- SearchTableViewController.m
+++ SearchTableViewController.m
@@ -14,6 +14,8 @@
 
 @interface SearchTableViewController ()
 @property (strong, nonatomic) NSArray *ingredients;
+@property (strong, nonatomic) NSMutableArray *safeIngredients;
+@property (strong, nonatomic) NSMutableArray *riskIngredients;
 @end
 NSString static *cellIdentifier = @"ExpiringCell";
 
@@ -21,20 +23,46 @@ NSString static *cellIdentifier = @"ExpiringCell";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.safeIngredients = [[NSMutableArray alloc] init];
+    self.riskIngredients = [[NSMutableArray alloc] init];
+    
     PFQuery *query = [PFQuery queryWithClassName:@"RawIngredient"];
     [query includeKey:@"ingredient"];
     [MBProgressHUD showHUDAddedTo:self.navigationController.view animated:YES];
     [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        _ingredients = objects;
+        
+        for (PFObject *object in objects) {
+            
+            // 如果保存期限小於 2 天，加進建議進貨清單
+            if ( [self calculateRemainLifeFrom:object.createdAt ShelfLife:[object[@"ingredient"][@"shelfLife"] integerValue]] > 24*2 ) {
+                [self.safeIngredients addObject: object];
+            
+            }else {
+                
+                [self.riskIngredients addObject: object];
+            }
+        }
+        
+        NSLog(@"safe: %ld", self.safeIngredients.count);
+        NSLog(@"risk: %ld", self.riskIngredients.count);
+        
         [self.tableView reloadData];
         [MBProgressHUD hideHUDForView:self.navigationController.view animated:YES];
     }];
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     [self.tableView registerNib:[UINib nibWithNibName:@"ExpiringIngredientCell" bundle:nil] forCellReuseIdentifier:cellIdentifier];
+}
+
+- (void)splitIngredientIntoTwoArray:(NSArray *)originArray {
+    
+}
+
+- (NSInteger)calculateRemainLifeFrom:(NSDate *)createdDate ShelfLife:(NSInteger)shelfLife{
+    
+    NSInteger interval = [[NSDate date] timeIntervalSinceDate:createdDate] / 3600;
+    NSInteger remainedLife = shelfLife - interval;
+    return remainedLife;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -45,39 +73,112 @@ NSString static *cellIdentifier = @"ExpiringCell";
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    return 2;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [_ingredients count];
+    
+    if (section == 0) {
+        return [self.riskIngredients count];
+//        return 0;
+    } else if (section == 1) {
+        return [self.safeIngredients count];
+    }
+    return 0;
 }
 
+
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    ExpiringIngredientTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
-    PFObject *rawIngredient = _ingredients[indexPath.row];
-    cell.nameLabel.text = rawIngredient[@"name"];
-    PFObject *ingredient = rawIngredient[@"ingredient"];
-    cell.quantity.text = [NSString stringWithFormat:@"庫存：%@ %@", [rawIngredient[@"quantity"] stringValue], [IngreCategory unitMap:ingredient[@"category"]]];
-    NSInteger interval = [[NSDate date] timeIntervalSinceDate:rawIngredient.createdAt] / 3600;
-    NSInteger shelfLife = [ingredient[@"shelfLife"] integerValue];
-    NSInteger remainedLife = shelfLife - interval;
-    CGFloat ratio = (float)remainedLife / (float)shelfLife;
-    [cell.progressBar setProgress:ratio animated:YES];
-    UIColor *progressColor;
-    if (ratio > 0.5) {
-        progressColor = [UIColor colorWithRed:0.53 green:0.99 blue:0 alpha:1];
-    } else if (ratio > 0.3) {
-        progressColor = [UIColor colorWithRed:0.95 green:0.83 blue:0.27 alpha:1];
-    } else {
-        progressColor = [UIColor redColor];
+    
+    if (indexPath.section == 0) {
+        ExpiringIngredientTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+        PFObject *rawIngredient = self.riskIngredients[indexPath.row];
+        cell.nameLabel.text = rawIngredient[@"name"];
+        PFObject *ingredient = rawIngredient[@"ingredient"];
+        cell.quantity.text = [NSString stringWithFormat:@"庫存：%@ %@", [rawIngredient[@"quantity"] stringValue], [IngreCategory unitMap:ingredient[@"category"]]];
+        NSInteger interval = [[NSDate date] timeIntervalSinceDate:rawIngredient.createdAt] / 3600;
+        NSInteger shelfLife = [ingredient[@"shelfLife"] integerValue];
+        NSInteger remainedLife = shelfLife - interval;
+        CGFloat ratio = (float)remainedLife / (float)shelfLife;
+        [cell.progressBar setProgress:ratio animated:YES];
+        UIColor *progressColor;
+        if (ratio > 0.5) {
+            progressColor = [UIColor colorWithRed:0.53 green:0.99 blue:0 alpha:1];
+        } else if (ratio > 0.3) {
+            progressColor = [UIColor colorWithRed:0.95 green:0.83 blue:0.27 alpha:1];
+        } else {
+            progressColor = [UIColor redColor];
+        }
+        [cell.progressBar setPrimaryColor:progressColor];
+        cell.expiringTime.text = [NSString stringWithFormat:@"剩餘時間： %li 小時", (long)remainedLife];
+        return cell;
     }
-    [cell.progressBar setPrimaryColor:progressColor];
-    cell.expiringTime.text = [NSString stringWithFormat:@"剩餘時間： %li 小時", (long)remainedLife];
-    return cell;
+    if (indexPath.section == 1) {
+        ExpiringIngredientTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+        PFObject *rawIngredient = self.safeIngredients[indexPath.row];
+        cell.nameLabel.text = rawIngredient[@"name"];
+        PFObject *ingredient = rawIngredient[@"ingredient"];
+        cell.quantity.text = [NSString stringWithFormat:@"庫存：%@ %@", [rawIngredient[@"quantity"] stringValue], [IngreCategory unitMap:ingredient[@"category"]]];
+        NSInteger interval = [[NSDate date] timeIntervalSinceDate:rawIngredient.createdAt] / 3600;
+        NSInteger shelfLife = [ingredient[@"shelfLife"] integerValue];
+        NSInteger remainedLife = shelfLife - interval;
+        CGFloat ratio = (float)remainedLife / (float)shelfLife;
+        [cell.progressBar setProgress:ratio animated:YES];
+        UIColor *progressColor;
+        if (ratio > 0.5) {
+            progressColor = [UIColor colorWithRed:0.53 green:0.99 blue:0 alpha:1];
+        } else if (ratio > 0.3) {
+            progressColor = [UIColor colorWithRed:0.95 green:0.83 blue:0.27 alpha:1];
+        } else {
+            progressColor = [UIColor redColor];
+        }
+        [cell.progressBar setPrimaryColor:progressColor];
+        cell.expiringTime.text = [NSString stringWithFormat:@"剩餘時間： %li 小時", (long)remainedLife];
+        return cell;
+    }else{
+        return nil;
+    }
+    
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return 111.;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    switch (section) {
+        case 0:
+            return @"建議進貨清單";
+            break;
+            
+        case 1:
+            return @"庫存清單";
+            break;
+            
+        default:
+            return nil;
+            break;
+    }
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 30;
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayHeaderView:(UIView *)view forSection:(NSInteger)section
+{
+    
+    if (section != 3) {   //section 3 is just a image
+        
+        // Text Color & Background color
+        UITableViewHeaderFooterView *header = (UITableViewHeaderFooterView *)view;
+        header.textLabel.textColor = [UIColor orangeColor];
+        
+        header.contentView.backgroundColor = [UIColor colorWithRed:25/255 green:28/255 blue:43/255 alpha:0.8];
+    }
 }
 
 /*
